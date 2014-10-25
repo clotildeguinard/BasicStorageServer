958e11f970ed1cc54ad5c4ddb8ce88a10442e60c
clogclo <clotilde_guinard@hotmail.fr>
2014-10-25 22:33:59 +0200

    commented a few classes and methods

--- src/app_kvClient/KVClient.java
+++ src/app_kvClient/KVClient.java
@@ -1,7 +1,26 @@
 package app_kvClient;
 
+import java.io.IOException;
 
-public class KVClient {
+// NADIA
+public class KVClient extends Thread {
+	private boolean connected = false;
 
-
+	public void run() {
+	try {
+		/* create socket, connect to server and init streams */
+		// call Clotilde's function connect() via the KVCommInterface
+		while (connected) { 
+			//communicate 
+			// extend the API of milestone 1 to handle PUT and GET
+			// call Clotilde's functions put() and get() via the KVCommInterface
+		}
+//	}
+//		catch (IOException ex) {
+//			// handle gracefully
+	} finally {
+		/* close streams and socket */
+		// call Clotilde's function disconnect() via the KVCommInterface
+	}
+	}
 }
--- src/client/KVStore.java
+++ src/client/KVStore.java
@@ -3,8 +3,14 @@ package client;
 
 import common.messages.KVMessage;
 
+/**
+ * send requests to the server, requires call to the defined communication protocol (encode, decode)
+ * @author Clotilde
+ *
+ */
 public class KVStore implements KVCommInterface {
-
+	private final String address;
+	private final int port;
 	
 	/**
 	 * Initialize KVStore with address and port of KVServer
@@ -12,30 +18,39 @@ public class KVStore implements KVCommInterface {
 	 * @param port the port of the KVServer
 	 */
 	public KVStore(String address, int port) {
-		
+		this.address = address;
+		this.port = port;
 	}
 	
 	@Override
 	public void connect() throws Exception {
-		// TODO Auto-generated method stub
+		// copy and adapt from previous milestone
 		
 	}
 
 	@Override
 	public void disconnect() {
-		// TODO Auto-generated method stub
+		// copy and adapt from previous milestone
 		
 	}
 
 	@Override
 	public KVMessage put(String key, String value) throws Exception {
-		// TODO Auto-generated method stub
+		//protocol defined by nadia to translate into a string, e.g.: "PUT KEY VALUE"
+		//send this string with send() method copied and adapted from previous milestone
+		// send() will call marshalling
+		// then will call receive() method copied and adapted from previous milestone
+		// unmarshall and return the corresponding KVMessage
 		return null;
 	}
 
 	@Override
 	public KVMessage get(String key) throws Exception {
-		// TODO Auto-generated method stub
+		//protocol defined by nadia to translate into a string, e.g.: "GET KEY"
+		//send this string with send() method copied and adapted from previous milestone
+		// send() will call marshalling
+		// then will call receive() method copied and adapted from previous milestone
+		// unmarshall and return the corresponding KVMessage
 		return null;
 	}
 
