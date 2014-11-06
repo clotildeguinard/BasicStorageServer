67a7f156c5de9eec285afbaa7b6ca88e3fbb176a
clogclo <clotilde_guinard@hotmail.fr>
2014-11-06 17:18:08 +0100

    continue

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -7,14 +7,14 @@ import org.apache.log4j.Level;
 import app_kvServer.KVServer;
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import logger.LogSetup;
+import logger.ClientLogSetup;
 
 
 public class AllTests {
 
 	static {
 		try {
-			new LogSetup("logs/testing/test.log", Level.ERROR);
+			new ClientLogSetup("logs/testing/test.log", Level.ERROR);
 			new KVServer(50000, 10, "FIFO");
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/testing/MarshalTest.java
+++ src/testing/MarshalTest.java
@@ -24,17 +24,12 @@ public class MarshalTest extends TestCase {
 	
 	@Test
 	public void testUnmarshal() {
-<<<<<<< Updated upstream
+
 		KVMessage unmarshalledText = KVMessageImpl.unmarshal(text);
 		assertEquals(kvMessage.getKey(), unmarshalledText.getKey());
 		assertEquals(kvMessage.getValue(), unmarshalledText.getValue());
 		assertEquals(kvMessage.getStatus(), unmarshalledText.getStatus());
-=======
-//		KVMessageImpl unmarshalledText = KVMessageImpl.unmarshall(text);
-//		assertEquals(kvMessage.getKey(), unmarshalledText.getKey());
-//		assertEquals(kvMessage.getValue(), unmarshalledText.getValue());
-//		assertEquals(kvMessage.getStatus(), unmarshalledText.getStatus());
->>>>>>> Stashed changes
+
 	}
 
 	
